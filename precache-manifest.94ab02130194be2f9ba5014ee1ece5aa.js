75abd8fe6e81bf5af7c9f5cef421d0bf62a4988b
Deepak Bhardwaj <deepak.bhardwaj@publicissapient.com>
2020-08-15 20:20:34 +0530

    Updates

---
+++ precache-manifest.94ab02130194be2f9ba5014ee1ece5aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78092f5d9c572944a613bb23fd4087a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f5ebd119f706cc9ddce",
+    "url": "/static/css/main.aeab9202.chunk.css"
+  },
+  {
+    "revision": "877d071ef54986e52f4b",
+    "url": "/static/js/2.f58c06d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f58c06d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5ebd119f706cc9ddce",
+    "url": "/static/js/main.87504af5.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "1559cb77607e1c72f2f038108c205a3d",
+    "url": "/static/media/about-page-bg.1559cb77.svg"
+  }
+]);
